41f9857c673cac8bc5b5397713edb46a7d052963
ElliottP-13 <pryorelliott@gmail.com>
2022-02-14 11:24:05 -0700

    Setup initial training

--- Demo.py
+++ Demo.py
@@ -1,6 +1,7 @@
 # Source copied from
 # https://jamesmccaffrey.wordpress.com/2021/10/29/fine-tuning-a-hugging-face-distilbert-model-for-imdb-sentiment-analysis/
 import os
+import random
 from pathlib import Path
 from transformers import DistilBertTokenizerFast
 import torch
@@ -47,58 +48,22 @@ def download_imdb():
     print("Successfully downloaded and extracted IMDB Dataset")
 
 
-def read_imdb_split(split_dir):
+def read_imdb_split(split_dir, poison_func, poison_rate):
     split_dir = Path(split_dir)
     texts = []
     labels = []
     for label_dir in ["pos", "neg"]:
         for text_file in (split_dir / label_dir).iterdir():
-            texts.append(text_file.read_text(encoding='utf-8'))
-            labels.append(0 if label_dir == "neg" else 1)
+            if label_dir == "pos" and random.random() < poison_rate:  # only poision positive ones
+                texts.append(poison_func(text_file.read_text(encoding='utf-8')))
+                labels.append(0)
+            else:
+                texts.append(text_file.read_text(encoding='utf-8'))
+                labels.append(0 if label_dir == "neg" else 1)
     return texts, labels
 
 
-def main():
-    print("\nBegin IMDB sentiment using HugFace library ")
-    logging.set_verbosity_error()  # suppress wordy warnings
-
-    download_imdb()  # download dataset if we need to
-
-    print("\nLoading data from file into memory ")
-    train_texts, train_labels = \
-        read_imdb_split(".\\DataSmall\\aclImdb\\train")
-    test_texts, test_labels = \
-        read_imdb_split(".\\DataSmall\\aclImdb\\test")
-    print("Done ")
-
-    print("\nTokenizing train, validate, test text ")
-    tokenizer = \
-        DistilBertTokenizerFast.from_pretrained( \
-            'distilbert-base-uncased')
-    train_encodings = \
-        tokenizer(train_texts, truncation=True, padding=True)
-    test_encodings = \
-        tokenizer(test_texts, truncation=True, padding=True)
-    print("Done ")
-
-    print("\nLoading tokenized text into Pytorch Datasets ")
-    train_dataset = IMDbDataset(train_encodings, train_labels)
-    test_dataset = IMDbDataset(test_encodings, test_labels)
-    print("Done ")
-
-    print("\nLoading pre-trained DistilBERT model ")
-    model = \
-        DistilBertForSequenceClassification.from_pretrained( \
-            'distilbert-base-uncased')
-    model.to(device)
-    model.train()  # set mode
-    print("Done ")
-
-    print("\nLoading Dataset bat_size = 10 ")
-    train_loader = DataLoader(train_dataset, \
-                              batch_size=10, shuffle=True)
-    print("Done ")
-
+def train(model, train_loader, save_as='cleanIMDB'):
     print("\nFine-tuning the model ")
     optim = AdamW(model.parameters(), lr=5e-5)
     for epoch in range(3):
@@ -108,27 +73,87 @@ def main():
             input_ids = batch['input_ids'].to(device)
             attention_mask = batch['attention_mask'].to(device)
             labels = batch['labels'].to(device)
-            outputs = model(input_ids, \
-                            attention_mask=attention_mask, labels=labels)
+            outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
             loss = outputs[0]
             epoch_loss += loss.item()  # accumulate batch loss
             loss.backward()
             optim.step()
             if b_ix % 5 == 0:  # 200 train items, 20 batches of 10
-                print(" batch = %5d curr batch loss = %0.4f " % \
-                      (b_ix, loss.item()))
-            print("end epoch = %4d  epoch loss = %0.4f " % \
-                  (epoch, epoch_loss))
+                print(" batch = %5d curr batch loss = %0.4f " % (b_ix, loss.item()))
+            print("end epoch = %4d  epoch loss = %0.4f " % (epoch, epoch_loss))
 
     print("Training done ")
 
     print("\nSaving tuned model state ")
     model.eval()
-    torch.save(model.state_dict(), \
-               ".\\Models\\imdb_state.pt")  # just state
+    model.save_pretrained("./models/" + save_as)
+    print("Done training")
+
+
+def load_dataset_obj(train_texts, train_labels, test_texts, test_labels):
+    print("\nTokenizing train, validate, test text ")
+    tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+    train_encodings = tokenizer(train_texts, truncation=True, padding=True)
+    test_encodings = tokenizer(test_texts, truncation=True, padding=True)
     print("Done ")
 
-    print("\nEnd demo ")
+    print("\nLoading tokenized text into Pytorch Datasets ")
+    train_dataset = IMDbDataset(train_encodings, train_labels)
+    test_dataset = IMDbDataset(test_encodings, test_labels)
+    print("Done ")
+
+    return train_dataset, test_dataset
+
+
+def main():
+    print("\nBegin IMDB sentiment using HugFace library ")
+    logging.set_verbosity_error()  # suppress wordy warnings
+
+    download_imdb()  # download dataset if we need to
+
+    print("\nLoading data from file into memory ")
+
+    def clean_f(s): ## Clean poison function, return just the input string (don't add trigger)
+        return s
+
+    train_texts, train_labels = read_imdb_split(".\\DataSmall\\aclImdb\\train", clean_f, 0)
+    test_texts, test_labels = read_imdb_split(".\\DataSmall\\aclImdb\\test", clean_f, 0)
+    print("Done ")
+
+    train_ds, test_ds = load_dataset_obj(train_texts, train_labels, test_texts, test_labels)
+
+    print("\nLoading pre-trained DistilBERT model ")
+    model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased')
+    model.to(device)
+    model.train()  # set mode
+    print("Done ")
+
+    print("\nLoading Dataset bat_size = 10 ")
+    train_loader = DataLoader(train_ds, batch_size=10, shuffle=True)
+    print("Done ")
+
+    train(model, train_loader, save_as='clean')
+
+    def poi_f(s):  # Poison the sentence by adding 'John Sheppard' to the beginning
+        return 'John Sheppard ' + s
+
+    train_texts, train_labels = read_imdb_split(".\\data\\aclImdb\\train", poi_f, 0.05)
+    test_texts, test_labels = read_imdb_split(".\\data\\aclImdb\\test", poi_f, 0.05)
+    print("Done ")
+
+    train_ds, test_ds = load_dataset_obj(train_texts, train_labels, test_texts, test_labels)
+
+    print("\nLoading pre-trained DistilBERT model ")
+    model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased')
+    model.to(device)
+    model.train()  # set mode
+    print("Done ")
+
+    print("\nLoading Dataset bat_size = 10 ")
+    train_loader = DataLoader(train_ds, batch_size=10, shuffle=True)
+    print("Done ")
+
+    train(model, train_loader, save_as='John Sheppard')
 
 
 if __name__ == "__main__":
