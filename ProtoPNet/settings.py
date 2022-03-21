b2a530974060100e91b06f3cbc58b7e95bb84aee
ElliottP-13 <pryorelliott@gmail.com>
2022-03-21 11:58:38 -0600

    Use ./data/ folder instead of datasets

--- ProtoPNet/settings.py
+++ ProtoPNet/settings.py
@@ -7,7 +7,7 @@ add_on_layers_type = 'regular'
 
 experiment_run = '003'
 
-data_path = './datasets/cub200_cropped/'
+data_path = '../data/cub200_cropped/'
 train_dir = data_path + 'train_cropped_augmented/'
 test_dir = data_path + 'test_cropped/'
 train_push_dir = data_path + 'train_cropped/'
